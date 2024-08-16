85b449c244ceb04ea63b0a2f233e05705e2aa4d5
Alejandro Chavez Nuñez <fullstack@naranti.com.mx>
2024-08-16 17:43:51 -0600

    create tenant

--- resources/views/tenants/create.blade.php
+++ resources/views/tenants/create.blade.php
@@ -1,1 +1,32 @@
-<x-app-layout></x-app-layout>
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Registrar inquilino') }}
+        </h2>
+    </x-slot>
+
+    <x-container class="py-10">
+        <div class="bg-white shadow p-10">
+                <form action="{{route('tenants.store')}}" method="POST">
+                    @csrf
+                    <div class="mb-4">
+                        <input-label>
+                            Nombre
+                        </input-label>
+                        <x-text-input name="id" class="w-full mt-2" placeholder="Ingrese el nombre"></x-text-input>
+                        @if ($errors->any())
+                            @foreach ($errors->all() as $error)
+                               <x-input-error :messages="$error"/>
+                            @endforeach
+                        @endif
+                    </div>
+                    <div class="flex justify-end">
+                        <button class="btn btn-blue">
+                            Guardar
+                        </button>
+                    </div>
+                </form>
+        </div>
+    </x-container>
+
+</x-app-layout>
--- resources/views/tenants/index.blade.php
+++ resources/views/tenants/index.blade.php
@@ -1,1 +1,17 @@
-<x-app-layout></x-app-layout>
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Inquilinos') }}
+        </h2>
+    </x-slot>
+    
+    <x-container class="py-10">
+
+        <div class="flex justify-end">
+            <a href={{route('tenants.create')}} class="btn btn-blue">
+                Nuevo
+            </a>
+        </div>
+    </x-container>
+
+</x-app-layout>
